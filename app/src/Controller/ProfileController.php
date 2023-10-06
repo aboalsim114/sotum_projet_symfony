86a5caa392716a3bb7d09be79b859110a324fd2e
IBRAHIM-ABDOULAYE <ibrahimkirni@gmail.com>
2023-10-06 18:22:28 +0200

    systeme Mise à jour de  profil

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProfileController extends AbstractController
 {
@@ -16,5 +18,30 @@ class ProfileController extends AbstractController
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
+    }
+
+
+
+    #[Route("/profile/update", name: "profile_update", methods: ["POST"])]
+    public function updateProfile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser(); // Récupère l'utilisateur connecté
+
+        // Récupérez les données du formulaire
+        $username = $request->request->get('username');
+        $email = $request->request->get('email');
+
+
+        // Mettez à jour l'entité User
+        $user->setUsername($username);
+        $user->setEmail($email);
+
+        // Enregistrez les modifications
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Redirigez vers la page de profil avec un message de confirmation
+        $this->addFlash('success', 'Profil mis à jour avec succès !');
+        return $this->redirectToRoute('app_profile');
     }
 }
