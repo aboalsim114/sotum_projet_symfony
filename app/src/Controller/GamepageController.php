90e8613714ababe96f724d05aa77c135c9d10bd0
aboalsim114 <aboalsim114@gmail.com>
2023-10-07 13:51:26 +0200

    update gamePage

--- app/src/Controller/GamepageController.php
+++ app/src/Controller/GamepageController.php
@@ -3,12 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class GamepageController extends AbstractController
 {
+    private $validPhrases = [
+        "plongeon",
+        "travaux",
+        "Chocolat",
+        "tendance",
+        "Chocolat",
+        "tendance",
+
+    ];
+
+    private function getLastValidatedRow(): int
+    {
+        $session = $this->get('session');
+        $validatedRows = $session->get('validatedRows', []);
+
+        // Retourne le dernier index validé ou -1 si aucun n'est validé.
+        return end($validatedRows) ?: -1;
+    }
+
+
     #[Route('/gamepage', name: 'app_gamepage')]
     public function index(): Response
     {
@@ -17,6 +39,31 @@ class GamepageController extends AbstractController
             'controller_name' => 'GamepageController',
             'rows' => 6,
             'columns' => 8,
+            'validPhrases' => $this->validPhrases,
+            'lastValidatedRow' => $this->getLastValidatedRow()
+
         ]);
+    }
+
+    #[Route('/validate-row', name: 'validate_row', methods: ["POST"])]
+    public function validateRow(Request $request): Response
+    {
+        $rowData = $request->request->get('row_data');
+        $rowIndex = (int) $request->request->get('row_index');
+
+        if (isset($this->validPhrases[$rowIndex])) {
+            $isValid = $rowData === $this->validPhrases[$rowIndex];
+        } else {
+            $isValid = false;
+        }
+
+        if ($isValid) {
+            $session = $this->get('session');
+            $validatedRows = $session->get('validatedRows', []);
+            $validatedRows[] = $rowIndex;
+            $session->set('validatedRows', $validatedRows);
+        }
+
+        return new JsonResponse(['isValid' => $isValid]);
     }
 }
--- app/templates/scripts/index.js
+++ app/templates/scripts/index.js
@@ -1,25 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const squares = document.querySelectorAll(".blue-square");
+document.addEventListener("DOMContentLoaded", function() {
+    const squares = document.querySelectorAll(".blue-square");
+    const columns = 8;
+    let currentRowIndex = 0;
 
-  squares.forEach((square, index) => {
-    // Écoutez les événements de saisie
-    square.addEventListener("input", function () {
-      // Assurez-vous qu'il n'y a qu'une seule lettre dans le carré
-      if (this.innerText.length > 1) {
-        this.innerText = this.innerText.charAt(0);
-      }
+    function isRowComplete(rowIndex) {
+        const startIndex = rowIndex * columns;
+        for (let i = startIndex; i < startIndex + columns; i++) {
+            if (!squares[i].innerText.trim()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-      // Si ce carré a une lettre, concentrez-vous sur le carré suivant
-      if (this.innerText.length === 1 && squares[index + 1]) {
-        squares[index + 1].focus();
-      }
+    function validateRowWithServer(rowData, rowIndex, callback) {
+        fetch('/validate-row', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'row_data=' + encodeURIComponent(rowData) + '&row_index=' + rowIndex
+            })
+            .then(response => response.json())
+            .then(data => {
+                callback(data.isValid);
+            });
+    }
+
+    squares.forEach((square, index) => {
+        square.addEventListener("input", function() {
+            if (this.innerText.length > 1) {
+                this.innerText = this.innerText.charAt(0);
+            }
+
+            if (this.innerText.length === 1 && squares[index + 1]) {
+                squares[index + 1].focus();
+            }
+        });
+
+        square.addEventListener("keydown", function(event) {
+            if (event.key === "Delete") {
+                this.innerText = "";
+                if (index > 0) {
+                    squares[index - 1].focus();
+                }
+                event.preventDefault();
+            }
+        });
+
+        square.addEventListener("focus", function() {
+            const rowIndex = Math.floor(index / columns);
+            if (rowIndex !== currentRowIndex) {
+                this.blur();
+            }
+        });
+
+        square.addEventListener("blur", function() {
+            const rowIndex = Math.floor(index / columns);
+            if (isRowComplete(rowIndex)) {
+                const rowData = Array.from(squares).slice(rowIndex * columns, (rowIndex + 1) * columns).map(s => s.innerText).join("");
+                validateRowWithServer(rowData, rowIndex, function(isValid) {
+                    if (isValid) {
+                        currentRowIndex++;
+                    } else {
+                        alert("La ligne " + (rowIndex + 1) + " n'est pas valide.");
+                        squares[rowIndex * columns].focus();
+                    }
+                });
+            }
+        });
     });
-
-    // Empêchez la navigation vers le carré suivant si celui-ci est vide
-    square.addEventListener("focus", function () {
-      if (index > 0 && !squares[index - 1].innerText) {
-        squares[index - 1].focus();
-      }
-    });
-  });
 });
