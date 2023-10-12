7c110f99b40e3f52dd5b6b2ddc9ee766570d8d32
aboalsim114 <aboalsim114@gmail.com>
2023-10-12 19:02:15 +0200

    ajouter la logique modifier son mot de passe

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ProfileController extends AbstractController
 {
@@ -20,21 +21,30 @@ class ProfileController extends AbstractController
         ]);
     }
 
-
-
     #[Route("/profile/update", name: "profile_update", methods: ["POST"])]
-    public function updateProfile(Request $request, EntityManagerInterface $entityManager): Response
+    public function updateProfile(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+
     {
         $user = $this->getUser(); // Récupère l'utilisateur connecté
 
         // Récupérez les données du formulaire
         $username = $request->request->get('username');
         $email = $request->request->get('email');
-
+        $password = $request->request->get('password');
+        $confirmPassword = $request->request->get('confirm_password');
 
         // Mettez à jour l'entité User
         $user->setUsername($username);
         $user->setEmail($email);
+
+        if ($password && $confirmPassword) {
+            if ($password !== $confirmPassword) {
+                $this->addFlash('error', 'The passwords do not match.');
+                return $this->redirectToRoute('app_profile');
+            }
+    
+            $user->setPassword($passwordHasher->hashPassword($user, $password));
+        }
 
         // Enregistrez les modifications
         $entityManager->persist($user);
